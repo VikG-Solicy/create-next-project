b0796b0c558e076d030e80f0b416619b126dae10
Vik Gevorgyan <VikG-Solicy@github.com>
2022-11-07 17:58:47 +0400

    code refactoring

--- pages/Users/UserCards.js
+++ pages/Users/UserCards.js
@@ -1,14 +1,20 @@
-﻿import { useState } from 'react';
+﻿import { useEffect, useState } from 'react';
+import Link from "next/link";
 import { Box, Button, Card } from "@mui/material";
 import axios from "axios";
-import Link from "next/link";
 
 const UserCards = () => {
     const [cards, setCards] = useState();
     
+    useEffect(() => {
+        const store = localStorage.getItem('users');
+        if (store) setCards(JSON.parse(store));
+    }, []);
+    
     const getUsers = async () => {
         const users = await axios('https://jsonplaceholder.typicode.com/users');
         setCards(users.data);
+        localStorage.setItem('users', JSON.stringify(users.data));
     };
     
     return (
@@ -17,7 +23,7 @@ const UserCards = () => {
             <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
                 {cards && cards.map(c =>
                     <Card key={c.id} sx={{ m: 3, p: 2, width: 200, border: '2px solid #1976d2', textAlign: 'center' }}>
-                        <Link href={`/Users/UserCards/${c.id}`}>{c.name}</Link>
+                        <Link href={`/Users/${c.id}`}>{c.name}</Link>
                     </Card>,
                 )}
             </Box>
