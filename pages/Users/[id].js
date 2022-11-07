466fdb37e6615235e9ba8457855e8d7d8530a153
Vik Gevorgyan <VikG-Solicy@github.com>
2022-11-07 17:58:47 +0400

    code refactoring

--- pages/Users/[id].js
+++ pages/Users/[id].js
@@ -1,15 +1,15 @@
 ﻿import { useEffect, useState } from "react";
-import axios from "axios";
 import { useRouter } from "next/router";
 import { Card, Typography } from "@mui/material";
+import axios from "axios";
 
 const Details = () => {
     const [user, setUser] = useState();
     const { id } = useRouter().query;
-    console.log(id);
+    
     useEffect(() => {
         if (id) getUsers();
-    }, [id]);
+    }, []);
     
     const getUsers = async () => {
         const userData = await axios(`https://jsonplaceholder.typicode.com/users/${id}`);
@@ -18,7 +18,7 @@ const Details = () => {
     
     return user && (
         <Card sx={{ border: '1px solid #1976d2', color: '#1976d2', m: 2, p: 2, width: 300, textAlign: 'center' }}>
-            <Typography variant="h4" m="15px" fontWeight="bold">About {user.name}</Typography>
+            <Typography variant="h5" m="15px" fontWeight="bold">About {user.name}</Typography>
             <Typography>username &gt; {user?.username}</Typography>
             <Typography>email &gt; {user?.email}</Typography>
             <Typography>address &gt; {user?.address?.street}</Typography>
